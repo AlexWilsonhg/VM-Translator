62eb9afe2ad30051479e150424aeb3f7dc879f66
Alex <awilson.hg@gmail.com>
2019-02-03 21:27:58 -0500

    Added add/sub translations.

--- Translator.java
+++ Translator.java
@@ -9,62 +9,85 @@ class Translator
 		switch(instruction.get(0))
 		{
 			case "push":
-				translatedInstructions = TranslatePushPop(instruction);
+				translatedInstructions = TranslatePush(instruction);
 				break;
 			case "pop":
-				translatedInstructions = TranslatePushPop(instruction);
+				translatedInstructions = TranslatePop(instruction);
 				break;
+			case "add":
+				translatedInstructions = TranslateSubAdd(instruction);
+				break;
+			case "sub":
+				translatedInstructions = TranslateSubAdd(instruction);
 		}
 		return translatedInstructions;
 	}
 
-	private Vector<String> TranslatePushPop(Vector<String> instruction)
+	private Vector<String> TranslatePush(Vector<String> instruction)
 	{
 		Vector<String> translatedInstructions = new Vector<String>();
-		// push
-		if(instruction.get(0).equals("push"))
+
+		if(instruction.get(1).equals("constant"))
 		{
-			if(instruction.get(1).equals("constant"))
-			{
-				translatedInstructions.add("@" + instruction.get(2));
-				translatedInstructions.add("D = A");
-			}
-			else
-			{
-				translatedInstructions.add("@" + instruction.get(1));
-				translatedInstructions.add("D = M");
-				translatedInstructions.add("@" + instruction.get(2));
-				translatedInstructions.add("A = A+D");
-				translatedInstructions.add("D = M");
-			}
-			translatedInstructions.add("@sp");
-			translatedInstructions.add("A = M");
-			translatedInstructions.add("M = D");
-			translatedInstructions.add("@sp");
-			translatedInstructions.add("M = M+1");
-		// pop
+			translatedInstructions.add("@" + instruction.get(2));
+			translatedInstructions.add("D = A");
 		}
 		else
 		{
 			translatedInstructions.add("@" + instruction.get(1));
 			translatedInstructions.add("D = M");
 			translatedInstructions.add("@" + instruction.get(2));
-			translatedInstructions.add("D = D+A");
-			translatedInstructions.add("@R15");
+			translatedInstructions.add("A = D+A");
+			translatedInstructions.add("D = M");
+		}
+			translatedInstructions.add("@sp");
+			translatedInstructions.add("A = M");
 			translatedInstructions.add("M = D");
 			translatedInstructions.add("@sp");
-			translatedInstructions.add("M = M-1");
-			translatedInstructions.add("A = M");
-			translatedInstructions.add("D = M");
-			translatedInstructions.add("@R15");
-			translatedInstructions.add("A = M");
-			translatedInstructions.add("M = D");
-		}
+			translatedInstructions.add("M = M+1");
+
 		return translatedInstructions;
 	}
 
-	private Vector<String> TranslateAdd(Vector<String> instruction)
+	private Vector<String> TranslatePop(Vector<String> instruction)
 	{
-		return null;
+		Vector<String> translatedInstructions = new Vector<String>();
+
+		translatedInstructions.add("@" + instruction.get(1));
+		translatedInstructions.add("D = M");
+		translatedInstructions.add("@" + instruction.get(2));
+		translatedInstructions.add("D = D+A");
+		translatedInstructions.add("@R15");
+		translatedInstructions.add("M = D");
+		translatedInstructions.add("@sp");
+		translatedInstructions.add("M = M-1");
+		translatedInstructions.add("A = M");
+		translatedInstructions.add("D = M");
+		translatedInstructions.add("@R15");
+		translatedInstructions.add("A = M");
+		translatedInstructions.add("M = D");
+
+		return translatedInstructions;
+	}
+
+	private Vector<String> TranslateSubAdd(Vector<String> instruction)
+	{
+		Vector<String> translatedInstructions = new Vector<String>();
+
+		translatedInstructions.add("@sp");
+		translatedInstructions.add("M = M-1");
+		translatedInstructions.add("A = M");
+		translatedInstructions.add("D = M");
+		translatedInstructions.add("A = A-1");
+		if(instruction.get(0).equals("sub"))
+		{
+			translatedInstructions.add("M = M-D");
+		}
+		else
+		{
+			translatedInstructions.add("M = M+D");
+		}
+
+		return translatedInstructions;
 	}
 }
--- VMTranslator.java
+++ VMTranslator.java
@@ -20,5 +20,3 @@ class VMTranslator
 		fileIO.Shutdown();
 	}
 }
-
-
